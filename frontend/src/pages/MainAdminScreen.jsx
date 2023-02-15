230b5253ac0ef82588a6f4d757ac4c631143ab35
Igor Brizack <igor_brizack@hotmail.com>
2023-02-15 08:18:47 -0300

    refactor: admin screen

---
+++ frontend/src/pages/MainAdminScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function MainAdminScreen() {
+  return (
+    <div>
+      <Header />
+      <h1>Olá</h1>
+    </div>
+  );
+}
+
+export default MainAdminScreen;
