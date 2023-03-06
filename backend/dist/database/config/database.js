ab664489361217836e21438f3d9307d04546ab44
Igor Brizack <igor_brizack@hotmail.com>
2023-03-06 11:18:27 -0300

    fix: admin users list / render

--- backend/dist/database/config/database.js
+++ backend/dist/database/config/database.js
@@ -5,7 +5,7 @@ require("dotenv/config");
 const config = {
     username: (_a = process.env.DB_USER) !== null && _a !== void 0 ? _a : 'root',
     password: (_b = process.env.DB_PASS) !== null && _b !== void 0 ? _b : '123456',
-    database: 'Gastos_DB',
+    database: 'gastos_app_db',
     host: (_c = process.env.DB_HOST) !== null && _c !== void 0 ? _c : 'localhost',
     port: (_d = Number(process.env.DB_PORT)) !== null && _d !== void 0 ? _d : 3002,
     dialect: 'mysql',
